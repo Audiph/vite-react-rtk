9f451d06cd9c80c37e1f4668574b8eafca6be9c1
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-09 03:44:22 +0800

    remove unused import

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from './CartItem';
-import { clearCart } from '../../features/cart/cartSlice';
 import { openModal } from '../../features/modal/modalSlice';
 
 const CartContainer = () => {

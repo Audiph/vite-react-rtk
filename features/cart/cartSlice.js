9f3a7f9780632e58f7925ab67e033f5f866fd5c1
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-09 03:13:52 +0800

    integrate increase and decrease amount

--- features/cart/cartSlice.js
+++ features/cart/cartSlice.js
@@ -21,9 +21,19 @@ const cartSlice = createSlice({
         (item) => item.id !== action.payload
       );
     },
+
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount++;
+    },
+
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount--;
+    },
   },
 });
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
